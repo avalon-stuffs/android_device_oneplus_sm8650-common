e4bc9f6ded14acc1c2e601745a86e1e3264ae3eb
Arian <arian.kulmer@web.de>
2024-06-08 22:14:30 +0530

    sm8650-common: Provide a static lib for power mode extension
    Change-Id: If4ec8af86d00cc4f7c0bd2e86e42c343b9e354fb
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -329,6 +329,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-oplus-sm8650)
+
 # Performance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint_pineapple.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_pineapple.json \
