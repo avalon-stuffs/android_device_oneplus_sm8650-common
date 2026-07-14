b634cc45e821609d823f3f8675d299d6aafb4c8b
LuK1337 <priv.luk@gmail.com>
2026-07-14 23:01:04 +0200

    sm8650-common: Let some qcom stuff use `lseek` syscall
    E ATFWD-daemon: libminijail[23175]: blocked syscall: lseek
    E tcmd    : libminijail[3293]: blocked syscall: lseek
    
    Change-Id: I6f39aa4152e31da7a07fdab35cffcfb572fd59bf

--- extract-files.py
+++ extract-files.py
@@ -78,6 +78,12 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('blob-patches/WfdCommon.patch'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    (
+        'system_ext/etc/seccomp_policy/tcmd.policy',
+        'vendor/etc/seccomp_policy/atfwd@2.0.policy',
+        'vendor/etc/seccomp_policy/qsap_qapeservice.policy',
+    ): blob_fixup()
+        .add_line_if_missing('lseek: 1'),
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'vendor/bin/init.kernel.post_boot-memory.sh': blob_fixup()
